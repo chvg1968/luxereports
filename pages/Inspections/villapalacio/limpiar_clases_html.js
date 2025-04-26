1a1ac6947a4e2f1eec7906a95d94138e3d8cceb9
chvg1968 <conradovilla@gmail.com>
2025-04-26 10:13:20 -0500

    Update all files

---
+++ pages/Inspections/villapalacio/limpiar_clases_html.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Emula __dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const inputFile = path.resolve(__dirname, 'VillaPalacio.html');
+const outputFile = path.resolve(__dirname, 'VillaPalacio-limpio.html');
+
+if (!fs.existsSync(inputFile)) {
+  console.error('No se encontró el archivo de entrada:', inputFile);
+  process.exit(1);
+}
+
+let html = fs.readFileSync(inputFile, 'utf8');
+
+html = html.replace(
+  /<(ul|ol)([^>]*class="[^"]*\bstart\b[^>]*)>/gi,
+  (match, tag, attrs) => {
+    let cleanAttrs = attrs.replace(/\s*style="[^"]*text-indent:[^";]*;?[^"]*"/gi, '');
+    cleanAttrs = cleanAttrs.replace(/class="[^"]*"/gi, 'class="lista-inspeccion"');
+    return `<${tag}${cleanAttrs}>`;
+  }
+);
+
+html = html.replace(
+  /(<(ul|ol)[^>]*?)\s+style="[^"]*text-indent:[^";]*;?[^"]*"/gi,
+  '$1'
+);
+
+fs.writeFileSync(outputFile, html, 'utf8');
+console.log(`Archivo limpio generado como ${outputFile}`);
